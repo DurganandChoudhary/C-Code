d74eeaa874534a1422b62a4bb2010b61057e9648
DURGANAND CHOUDHARY <102054803+DurganandChoudhary@users.noreply.github.com>
2023-09-16 02:00:57 +0530

    Add files via upload

---
+++ demo11.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    int myNum = 150;
+    printf("%d",myNum);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ demo13.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int myNum=155;
+    float myFloatNum=5.666;
+    char myLetter = 'D';
+
+  printf("%d\n", myNum);
+  printf("%f\n", myFloatNum);
+  printf("%c\n", myLetter);
+  return 0;
+
+}
