9745b03e25b67c47ad5f1c88b2ca4d367b103ba6
DURGANAND CHOUDHARY <102054803+DurganandChoudhary@users.noreply.github.com>
2023-09-28 00:02:34 +0530

    Add files via upload

---
+++ PPTR.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    // float price = 100.00;
+    // float *pptr = &price;
+    // float **pptr = &ptr;
+
+    int i = 5;
+    int *ptr = &i;
+    int **pptr = &ptr;
+
+    printf("%d\n",**pptr);
+
+
+    return 0;
+}
