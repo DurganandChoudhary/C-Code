9e5ee20c0fbddc2541fb247633b9ccca6947455b
DURGANAND CHOUDHARY <102054803+DurganandChoudhary@users.noreply.github.com>
2023-10-01 19:28:10 +0530

    Add files via upload

---
+++ Complex.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+struct complex
+{
+    int real;
+    int img;
+
+};
+
+int main()
+{
+    struct complex num1 = {5,8};
+    struct complex *ptr = &num1;
+    printf("real part = %d\n",ptr->real);
+    printf("img part = %d\n",ptr->img);
+
+    return 0;
+}
