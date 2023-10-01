3f5b67dc0997693a5c5fb2f2b7da8ebc198b4fa8
DURGANAND CHOUDHARY <102054803+DurganandChoudhary@users.noreply.github.com>
2023-10-01 17:35:09 +0530

    Add files via upload

---
+++ PassStruct.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+
+struct student
+{
+    int roll;
+    float cgpa;
+    char name[100];
+};
+void printInfo(struct student s1);
+
+int main()
+{
+    struct student s1 = {5478,9.2,"Aman"};
+    //printf("Student.roll = %d\n",s1.roll);
+    printInfo(s1);
+
+    s1.roll = 1660; //change
+    printf("student.roll = %d\n",s1.roll);
+
+
+
+    return 0;
+}
+
+void printInfo(struct student s1)
+{
+    printf("Student Information\n");
+    printf("Student.roll = %d\n",s1.roll);
+    printf("student.name= %s\n",s1.name);
+    printf("Student.cgpa = %f\n",s1.cgpa);
+
+    
+}
