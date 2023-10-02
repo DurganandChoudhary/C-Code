366529a3c3f68833dfacee3a1be01ed6ab4308ed
DURGANAND CHOUDHARY <102054803+DurganandChoudhary@users.noreply.github.com>
2023-10-02 12:28:17 +0530

    Add files via upload

---
+++ FilePointer5.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+    FILE *fptr;
+    fptr = fopen("text.txt","r");
+    int n;
+    fscanf(fptr,"%d",&n);
+    printf("number = %d\n",n);
+
+    fscanf(fptr,"%d",&n);
+    printf("number = %d\n",n);
+
+    fscanf(fptr,"%d",&n);
+    printf("number = %d\n",n);
+
+    fscanf(fptr,"%d",&n);
+    printf("number = %d\n",n);
+
+    fscanf(fptr,"%d",&n);
+    printf("number = %d\n",n);
+
+    fclose(fptr);
+    return 0;
+}
