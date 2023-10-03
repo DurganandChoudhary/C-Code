f771e000ff7ab9a8bae9d2cb263bb700cc0ad254
DURGANAND CHOUDHARY <102054803+DurganandChoudhary@users.noreply.github.com>
2023-10-04 01:04:05 +0530

    Add files via upload

---
+++ Nsize.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int *ptr;
+    int n;
+    printf("Enter n : ");
+    scanf("%d",&n);
+
+    ptr = (int *) calloc(n, sizeof(int));
+
+    for(int i=0;i<n;i++)
+    {
+        printf("%d\n",ptr[i]);
+    }
+   
+    return 0;
+}
